92af91ee4ba57835acd9b244c3a22ec76c45e4d6
Dimitris Schizas <dschizas@azuruw.com>
2021-06-26 20:22:54 +0100

    repo first commit for docusaurus documentation

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/Users/dschizas/Documents/Github/documentation-boilerplate/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/dschizas/Documents/Github/documentation-boilerplate/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/dschizas/Documents/Github/documentation-boilerplate/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/dschizas/Documents/Github/documentation-boilerplate/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/dschizas/Documents/Github/documentation-boilerplate/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/dschizas/Documents/Github/documentation-boilerplate/src/css/custom.css"),
+];
